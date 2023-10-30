e45748f4552354550ab1278e123ec9be95bbf29e
Dave Naugler <davenaugler@gmail.com>
2023-10-30 17:54:52 -0600

    a little further in threads

--- src/com/coderscampus/lesson1/MultiThreadingApp.java
+++ src/com/coderscampus/lesson1/MultiThreadingApp.java
@@ -1,8 +1,18 @@
 package com.coderscampus.lesson1;
 
 public class MultiThreadingApp {
-    
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i < 5; i++) {
+            
+        }
+    }
+
+    class SomeTask implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Hey look at me, I'm a task! I'm running on thread: " + Thread.currentThread().getName());
+        }
     }
 }
