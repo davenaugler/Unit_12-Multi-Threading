b823a2c7047d8a7d9cedf74d86acf61af73e6721
Dave Naugler <davenaugler@gmail.com>
2023-11-01 06:37:11 -0600

    Completed '02 - Runnin a Task'

--- src/com/coderscampus/lesson1/MultiThreadingApp.java
+++ src/com/coderscampus/lesson1/MultiThreadingApp.java
@@ -4,15 +4,8 @@ public class MultiThreadingApp {
 
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            
-        }
-    }
-
-    class SomeTask implements Runnable {
-
-        @Override
-        public void run() {
-            System.out.println("Hey look at me, I'm a task! I'm running on thread: " + Thread.currentThread().getName());
+            SomeTask task = new SomeTask();
+            new Thread(task).start();
         }
     }
 }
--- src/com/coderscampus/lesson1/SomeTask.java
+++ src/com/coderscampus/lesson1/SomeTask.java
@@ -1,2 +1,8 @@
-package com.coderscampus.lesson1;public class SomeTask {
+package com.coderscampus.lesson1;
+
+public class SomeTask implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("Hey look at me, I'm a task! I'm running on thread: " + Thread.currentThread().getName());
+        }
 }
