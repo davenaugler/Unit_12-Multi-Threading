78e79207466e20399e3ab47036cd6a2ee27f83b9
Dave Naugler <davenaugler@gmail.com>
2023-11-02 10:34:29 -0600

    progressing through threads

--- src/com/coderscampus/lesson1/MultiThreadingApp.java
+++ src/com/coderscampus/lesson1/MultiThreadingApp.java
@@ -1,11 +1,17 @@
 package com.coderscampus.lesson1;
 
 public class MultiThreadingApp {
-
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        String message = "Starting";
+        System.out.println(message);
+        System.out.println(" Thread-" + Thread.currentThread().getName());
+
+        for (int i = 0; i < 11; i++) {
             SomeTask task = new SomeTask();
             new Thread(task).start();
         }
+
+        message = "Done";
+        System.out.println(message);
     }
 }
--- src/com/coderscampus/lesson1/SomeTask.java
+++ src/com/coderscampus/lesson1/SomeTask.java
@@ -1,8 +1,19 @@
 package com.coderscampus.lesson1;
 
+import java.util.Random;
+
 public class SomeTask implements Runnable {
+
+    static final double INT_MULTIPLIER = 1.5;
         @Override
         public void run() {
+
+            for (int i = 0; i < 1000000; i++) {
+                Integer randomInt_1 = new Random().nextInt();
+                Integer randomInt_2 = new Random().nextInt();
+                Double randomValue = (randomInt_1 * randomInt_2) * INT_MULTIPLIER;
+//                System.out.println("Random Value: " + randomValue);
+            }
             System.out.println("Hey look at me, I'm a task! I'm running on thread: " + Thread.currentThread().getName());
         }
 }
