3d05d4507cab5af8fc584b3cb718552779403f61
Dave Naugler <davenaugler@gmail.com>
2023-11-04 15:22:28 -0600

    adding content to Unit 12 - MultiThreading

--- src/com/coderscampus/lesson1/MultiThreadingApp.java
+++ src/com/coderscampus/lesson1/MultiThreadingApp.java
@@ -1,15 +1,34 @@
 package com.coderscampus.lesson1;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MultiThreadingApp {
     public static void main(String[] args) {
         String message = "Starting";
         System.out.println(message);
         System.out.println(" Thread-" + Thread.currentThread().getName());
 
-        for (int i = 0; i < 11; i++) {
-            SomeTask task = new SomeTask();
-            new Thread(task).start();
+        // * This starts up as many threads as there are iterations in the for loop.
+        // * You only want to fire up a ton of threads, if those threads are going to
+        // * be waiting a lot... i.e. sending requests across the internet.
+//        for (int i = 0; i < 11; i++) {
+//            SomeTask task = new SomeTask();
+//            new Thread(task).start();
+//        }
+
+        // * For a CPU bound operation like we have in our "SomeTask" class,
+        // * we should make use of an ExecutorService
+
+        // # .newFixedThreadPool vs .newCachedThreadPool
+        // # With Fixed you need to specify the number of threads
+        // # With Cached you do not need to specify the number of threads
+
+        ExecutorService service = Executors.newCachedThreadPool();
+        for (int i = 0; i < 50; i++) {
+            service.execute(new SomeTask());
         }
+
 
         message = "Done";
         System.out.println(message);
--- src/com/coderscampus/lesson1/SomeTask.java
+++ src/com/coderscampus/lesson1/SomeTask.java
@@ -1,19 +1,22 @@
 package com.coderscampus.lesson1;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Random;
 
 public class SomeTask implements Runnable {
-
+    private static int taskID = 0;
     static final double INT_MULTIPLIER = 1.5;
-        @Override
-        public void run() {
 
-            for (int i = 0; i < 1000000; i++) {
-                Integer randomInt_1 = new Random().nextInt();
-                Integer randomInt_2 = new Random().nextInt();
-                Double randomValue = (randomInt_1 * randomInt_2) * INT_MULTIPLIER;
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000000; i++) {
+            Integer randomInt_1 = new Random().nextInt();
+            Integer randomInt_2 = new Random().nextInt();
+            Double randomValue = (randomInt_1 * randomInt_2) * INT_MULTIPLIER;
 //                System.out.println("Random Value: " + randomValue);
-            }
-            System.out.println("Hey look at me, I'm a task! I'm running on thread: " + Thread.currentThread().getName());
         }
+        taskID++;
+        System.out.println("Hey look at me, I'm taskID: " + taskID + "! I'm running on thread: " + Thread.currentThread().getName());
+    }
 }
