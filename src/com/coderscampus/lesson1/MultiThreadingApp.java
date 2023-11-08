ba223efee1bab56ef61cc269505d3809a63442d1
Dave Naugler <davenaugler@gmail.com>
2023-11-08 16:32:13 -0700

    Adding some new content to Unit 12

--- src/com/coderscampus/lesson1/MultiThreadingApp.java
+++ src/com/coderscampus/lesson1/MultiThreadingApp.java
@@ -1,13 +1,15 @@
 package com.coderscampus.lesson1;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MultiThreadingApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         String message = "Starting";
-        System.out.println(message);
-        System.out.println(" Thread-" + Thread.currentThread().getName());
+        System.out.println(message); // + Starting
+        System.out.println(" Thread-" + Thread.currentThread().getName()); // + Thread-main
 
         // * This starts up as many threads as there are iterations in the for loop.
         // * You only want to fire up a ton of threads, if those threads are going to
@@ -20,17 +22,51 @@ public class MultiThreadingApp {
         // * For a CPU bound operation like we have in our "SomeTask" class,
         // * we should make use of an ExecutorService
 
-        // # .newFixedThreadPool vs .newCachedThreadPool
+        // # .newFixedThreadPool vs .newCachedThreadPool // Use on CPU bound situations
         // # With Fixed you need to specify the number of threads
-        // # With Cached you do not need to specify the number of threads
+        // + With Cached you do not need to specify the number of threads // Use with I/O or HTTP requests
+        // +   CachedThreadPool there is not theoretically maximum really
+        // +   It will continue to create new threads to support however many threads you need
+        // +   What will happen. As the tasks finish, if the thread remains idle for 60 seconds it will
+        // +   shut down that thread.
+        // # Don't use a newCachedThreadPool when you are working with a CPU bound task because
+        // # if were to run this you will see that it will quickly start up as many threads as it needs
+        // # taxing the CPU extremely hard.
 
-        ExecutorService service = Executors.newCachedThreadPool();
+        // ? IF this were a more I/O or HTTP request environment where you are waiting a lot, where it's not a CPU task.
+        // ? It alleviates the guessing game on how many threads would be needed.
+
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
         for (int i = 0; i < 50; i++) {
-            service.execute(new SomeTask());
+//            service.execute(new SomeTask());
+            Future<TaskDto> futureTask = service.submit(new SomeTask());
+            System.out.println("Future Task: " + i + " " + futureTask.get()); // + All 'Future Task's' (50 of them)
         }
-
-
         message = "Done";
-        System.out.println(message);
+        System.out.println(message); // + Done
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/com/coderscampus/lesson1/SomeTask.java
+++ src/com/coderscampus/lesson1/SomeTask.java
@@ -3,20 +3,26 @@ package com.coderscampus.lesson1;
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.Random;
+import java.util.concurrent.Callable;
 
-public class SomeTask implements Runnable {
+public class SomeTask implements Callable<TaskDto> {
+// * Dto stands for data transfer object
     private static int taskID = 0;
     static final double INT_MULTIPLIER = 1.5;
 
     @Override
-    public void run() {
+    public TaskDto call() throws Exception {
+        TaskDto taskDto = new TaskDto();
         for (int i = 0; i < 1000000; i++) {
             Integer randomInt_1 = new Random().nextInt();
             Integer randomInt_2 = new Random().nextInt();
-            Double randomValue = (randomInt_1 * randomInt_2) * INT_MULTIPLIER;
+            Double something = (randomInt_1 * randomInt_2) * INT_MULTIPLIER;
+            taskDto.setValue(something);
 //                System.out.println("Random Value: " + randomValue);
         }
         taskID++;
-        System.out.println("Hey look at me, I'm taskID: " + taskID + "! I'm running on thread: " + Thread.currentThread().getName());
+        System.out.println("Hey look at me, I'm taskID: " + taskID + "! I'm running on thread: " + Thread.currentThread().getName()); // + Hey look at me,... 50 times
+        taskDto.setFinished(true);
+        return taskDto;
     }
 }
